50e473ef7f0e6ac34e6c7d5125f4eac958d5093a
kimiyazargari <kimiyazargari13@gmail.com>
2026-01-13 22:01:03 +0100

    add main trainer

--- src/trainers/__init__.py
+++ src/trainers/__init__.py
@@ -5,9 +5,11 @@ Training modules for different joke datasets
 from .conan_jokes_trainer import main as train_conan
 from .general_jokes_trainer import main as train_general
 from .reddit_jokes_trainer import main as train_reddit
+from .joke_trainer import main as train_model
 
 __all__ = [
     "train_conan",
     "train_general",
     "train_reddit",
+    "train_model",
 ]
